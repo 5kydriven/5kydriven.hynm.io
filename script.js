bca90de6a7a34cf4665b7e2d3f7e27169f5208ef
5kydriven <113752690+5kydriven@users.noreply.github.com>
2023-02-11 14:11:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.getElementById("yes").addEventListener("click", function(){alert("See y
 var b = document.getElementById("no")
 b.addEventListener("click",change);
 function change() {
-    var i = Math.floor(Math.random()*1000)+1;
-    var j = Math.floor(Math.random()*700)+1;
+    var i = Math.floor(Math.random()*400)+1;
+    var j = Math.floor(Math.random()*400)+1;
     b.style.left = i + "px";
     b.style.top = j + "px";
 }
