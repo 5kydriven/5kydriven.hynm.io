d34b29531b33354cc5ed839c522f67a90550b2b1
5kydriven <113752690+5kydriven@users.noreply.github.com>
2023-03-30 06:06:57 +0800

    Update script.js

--- designFunctions/script.js
+++ designFunctions/script.js
@@ -7,11 +7,13 @@ alert('Warning! Audio Highly Addicted');
 
 btn.addEventListener('click', () =>{
     x.play();
+    btn.classList.add('show');
     modal.classList.add('show');
 })
 
 xbtn.addEventListener('click', () => {
     modal.classList.remove('show');
+    btn.classList.remove('show');
     btn.innerHTML = 'April Fools';
     btn.style.pointerEvents = 'none';
 })
