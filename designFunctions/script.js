29ceac4c841b434587fd7b931d81a87ea50f57d3
5kydriven <113752690+5kydriven@users.noreply.github.com>
2023-03-29 22:16:20 +0800

    Add files via upload

---
+++ designFunctions/script.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("myAudio");
+var modal = document.getElementById('container');
+var btn = document.getElementById('btn');
+var xbtn = document.getElementById('xbtn');
+
+alert('Warning! Audio Highly Addicted');
+
+btn.addEventListener('click', () =>{
+    x.play();
+    modal.classList.add('show');
+})
+
+xbtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+    btn.innerHTML = 'April Fools';
+    btn.style.pointerEvents = 'none';
+})
